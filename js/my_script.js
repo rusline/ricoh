b5ddc70a902aa668a55e1d5faaa9f11fef6493ac
Rusline <salikov.rusline@gmail.com>
2013-04-14 23:51:47 +0600

    reg page

--- js/my_script.js
+++ js/my_script.js
@@ -4,11 +4,13 @@
     $('input:text, input:password').each(function(){
         var txtval = $(this).val();
         $(this).focus(function(){
+						$(this).css({'color':'#000', 'font-style':'normal'});
             if($(this).val() == txtval){
                 $(this).val('')
             }
         });
         $(this).blur(function(){
+						$(this).css({'color':'#000', 'font-style':'normal'});
             if($(this).val() == ""){
                 $(this).val(txtval);
             }
@@ -36,35 +38,31 @@
 			  }
 
     });
-	
+		
+		//form
+		$('.line select, .line input').styler();
+		
 		// Slider
 		var i = $(window).width();
-			if (i > 319){ $('div.slide').css({ width: i }); }
-				$("#slider").scrollable({ circular: true, touch: false, easing: 'easeInOutBack', speed: 900}).navigator({ navi: '#navigation' });
-				window.api = $("#slider").data("scrollable");
+			if (i > 319){ $('#items > div').css({ width: i }); }
+			$("#slider").scrollable({ circular: true, touch: false, easing: 'easeInOutBack', speed: 900}).navigator({ navi: '#navigation' });
+			window.api = $("#slider").data("scrollable");
 			$(window).resize(function() {
 				var a = 1 + window.api.getIndex();
 				var w = $(window).width();
 				if (w > 319) {
 					var l = a * w
 					$('#items').css({ left: + - +l });
-					$('div.slide').css({ width: w });
+					$('#items > div').css({ width: w });
 				} else {
-					$('div.slide').css({ width: 300 });
+					$('#items > div').css({ width: 300 });
 				}
 			});
-	
 
-	var params = {
-    changedEl: ".lineForm select"
-    }
-    cuSel(params);
-	
-	
-
-
+		
 
 };
 
 $(func);
+
 
