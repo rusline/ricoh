b7719aa10aeeadcdf564682d5d0b050ebbe110e4
Rusline <salikov.rusline@gmail.com>
2013-04-25 15:59:22 +0600

    fix 3

--- js/my_script.js
+++ js/my_script.js
@@ -21,18 +21,47 @@
 		$('.enter-link').each(function(i, item){
 			$(item).bind('mouseover', function(e){
 			 e.preventDefault();
-			 $('div.enter-drop').toggle();
+			 $('div.enter-drop').show();
 			})
+		});
+		
+		var n = 0;
+		$("div.enter-drop").bind("mouseenter",function(){
+			$(this).show();
+		}).bind("mouseleave",function(){
+			$(this).hide();
+		});
+			
+		
+		
+		// delete
+		$('.del').click(function(){
+				$(this).parents(".serial-list li").animate({ opacity: 'hide' }, "slow");
+		});
+		
+		// message
+		$('.message-box span').click(function(){
+				$(this).parents(".message-box div").animate({ opacity: 'hide' }, "slow");
+		});
+		
+		
+		// faq
+		$(".support-full div.s-top").click(function(){
+			$(this).next("div.s-bot").slideToggle("slow")
+			.siblings("div.s-bot:visible").slideUp("slow");
+			$(this).toggleClass("active");
+			$(this).siblings("div.s-top").removeClass("active");
+			return false;
 		});
 		
 		
 		// popup
 		$('.popup-link').click(function(){
 				$('.overlay').fadeIn('fast',function(){
-						$('.popup-box').animate({'top':'300px'},500);
+						$('.popup-box').animate({'top':'250px'},500);
 				});
 		});
-		$('.close-popup').click(function(){
+		$('.close-popup, .overlay').click(function(){
 				$('.popup-box').animate({'top':'-2000px'},500,function(){
 						$('.overlay').fadeOut('fast');
 				});
